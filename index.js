fba4083e7fe781058647663f11bd7c0fcded9ba3
LiHua Anderson <lihua.anderson@gmail.com>
2021-04-09 13:30:25 -0700

    added classes for manager, intern and engineer

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ const fs = require('fs');
 const employee = require('./lib/Employee.js');
 const manager = require('./lib/Manager.js');
 const engineer = require('./lib/Engineer.js');
-const intern = require(./lib/Intern.js);
+const intern = require('./lib/Intern.js');
+
+// function to initialize app
+function init() {
+    questions()
+        .then((data) => {
+            console.log(data);
+            writeToFile('./dist/index.html', generate(data))
+        });
+};
+
+// initialize app
+init();
