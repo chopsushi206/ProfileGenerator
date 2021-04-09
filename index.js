6010cb6754eb14aaf302c459ef186a68e92f080b
LiHua Anderson <lihua.anderson@gmail.com>
2021-04-09 14:03:20 -0700

    added functions to start program and a question that returns value

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const manager = require('./lib/Manager.js');
 const engineer = require('./lib/Engineer.js');
 const intern = require('./lib/Intern.js');
 
+const startApp = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'start',
+            message: 'Would you like to create profiles for your team?',
+            choices: ['Yes', 'No']
+        },
+    ]);
+};
+
 // function to initialize app
 function init() {
-    questions()
+    startApp()
         .then((data) => {
             console.log(data);
-            writeToFile('./dist/index.html', generate(data))
         });
 };
 
