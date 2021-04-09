dcd1a761ed7f0132e6e33059b3a74343c71956e6
LiHua Anderson <lihua.anderson@gmail.com>
2021-04-09 13:13:20 -0700

    added variables for required docs in index.js and added a class to Employee.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const employee = require('./lib/Employee.js');
+const manager = require('./lib/Manager.js');
+const engineer = require('./lib/Engineer.js');
+const intern = require(./lib/Intern.js);
